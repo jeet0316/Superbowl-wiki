b347b37cb23d5055ddec0ba6460eb4a38ddb242d
jeet0316 <107562607+jeet0316@users.noreply.github.com>
2023-04-05 12:05:10 -0400

    Done

--- program/solution.py
+++ program/solution.py
@@ -24,7 +24,7 @@ class Super_bowl_reasult:
         self.city = city
         self.state = state
 
-
+#3b adding items to the list
 def read_csv():
     with open("program/superbowl.csv", "r", encoding="utf-8") as nfl_file:
         reader = csv.reader(nfl_file, delimiter=",")
@@ -47,7 +47,7 @@ def read_csv():
                 print("The following has value error")
                 print(row)
 
-
+#3c_part_1
 def get_list_by_data(list_to_sort, sort_type, sort_attr):
     temp_list = []
     for bowl in list_to_sort:
@@ -56,7 +56,7 @@ def get_list_by_data(list_to_sort, sort_type, sort_attr):
                 temp_list.append(bowl)
     return temp_list
 
-
+#3b get attribute from the list
 def final_reasults(final_list):
     if len(final_list) < 1:
         print("No data available for the following search")
@@ -76,65 +76,15 @@ def start():
     while True:
         year = ""
         while year == "":
-            year = input("Please choose  year from 1967 to 2020: ")
-            if year not in [
-                "1967",
-                "1968",
-                "1969",
-                "1970",
-                "1971",
-                "1972",
-                "1973",
-                "1974",
-                "1975",
-                "1976",
-                "1977",
-                "1978",
-                "1979",
-                "1980",
-                "1981",
-                "1982",
-                "1983",
-                "1984",
-                "1985",
-                "1986",
-                "1987",
-                "1988",
-                "1989",
-                "1990",
-                "1991",
-                "1992",
-                "1993",
-                "1994",
-                "1995",
-                "1996",
-                "1997",
-                "1998",
-                "1999",
-                "2000",
-                "2001",
-                "2002",
-                "2003",
-                "2004",
-                "2005",
-                "2006",
-                "2007",
-                "2008",
-                "2009",
-                "2010",
-                "2011",
-                "2012",
-                "2013",
-                "2014",
-                "2015",
-                "2016",
-                "2017",
-                "2018",
-                "2019",
-                "2020",
-            ]:
-                print("Please only choose from 1967 to 2020")
+            year = input("Please choose  year from 1967 to 2022: ")
+            if year not in ["1967","1968","1969""1970","1971","1972","1973","1974","1975","1976","1977","1978",
+            "1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992",
+            "1993", "1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006",
+            "2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020",
+            "2021", "2022"]:
+                print("Please only choose from 1967 to 2022")
                 year = ""
+                #3c part 2/3d first call
         year_list = get_list_by_data(superbowl_data, "year", year)
         final_reasults(year_list)
         restart = input("Do you want to go again? [y/n]: ").lower()
