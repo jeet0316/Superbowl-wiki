2516af88fc814146efb9fd8caf7fdbe9a2b194c2
jeet0316 <107562607+jeet0316@users.noreply.github.com>
2023-04-05 13:38:47 -0400

    Done with Corrections

--- program/solution.py
+++ program/solution.py
@@ -1,5 +1,5 @@
 import csv
-
+from tabulate import tabulate
 superbowl_data = []
 
 
@@ -62,11 +62,12 @@ def final_reasults(final_list):
         print("No data available for the following search")
     else:
         for bowl in final_list:
-            print(f"Date of The Superbowl: {bowl.date}")
-            print(f"Location: {bowl.stadium}, {bowl.city}, {bowl.state}")
-            print(f"Winner of the SuperBowl: {bowl.winner} by {bowl.winpts}")
-            print(f"MVP: The MVP of the game was {bowl.mvp}")
-            print(f"Loser of the SuperBowl: {bowl.lose} by {bowl.losepts}")
+            print(f"Date of The Superbowl:          {bowl.date}")
+            print(f"Location:                       {bowl.stadium}, {bowl.city}, {bowl.state}")
+            print(f"Winner of the SuperBowl:        {bowl.winner} by {bowl.winpts}")
+            print(f"MVP:                            {bowl.mvp}")
+            print(f"Loser of the SuperBowl:         {bowl.lose} by {bowl.losepts}")
+            
 
 
 def start():
